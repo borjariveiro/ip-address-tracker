0733834a675026bc332ac4f1b510cceb6813967c
[Borja Riveiro Carracedo] <[borja.riveiro@hotmail.com]>
2021-08-20 22:10:48 +0200

    Add first part index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
-const b = "Hola"
+const form = document.querySelector("#searchForm")
+const input = document.querySelector("#searchInput")
+const button = document.querySelector("#searchButton")
+const ip = document.querySelector("#addressIP")
+const locationZP = document.querySelector("#addressLocation")
+const addressTimezone = document.querySelector("#addressTimezone")
+const isp = document.querySelector("#addressISP")
+const searchedIP = input
 
-console.log(b)
+async function getIpData(ip) {
+  const API = `https://geo.ipify.org/api/v1?apiKey=at_w4eNJTImofKjQATDpKSnhvV6wtYMG&ipAddress=${ip}`
+
+  try {
+    const ipRequest = await fetch(API)
+
+    if (!ipRequest.ok) {
+      throw new Error(ipRequest.status)
+    }
+
+    const ipData = await ipRequest.json()
+
+    console.log(ipData)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+// getIpData("192.212.174.101")
